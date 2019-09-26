5f758cc2ea7fb46bcad191663252d4429a27a59e
caco <crns13@gmail.com>
2019-09-26 20:31:00 -0300

    Creates two more tests

--- genes/tests/test_api.py
+++ genes/tests/test_api.py
@@ -8,12 +8,12 @@ class GengeAPITest(APITestCase):
 
     databases = {'default', 'remote'}
 
-    def test_get_returns_right_entries(self):
+    def test_get_returns_right_entries1(self):
         url = reverse('geneautocomplete-list', kwargs={
             'query': 'tmem18', 'species': 'danio_rerio', 'limit': 3
         })
-
         response = self.client.get(url)
+
         self.assertEqual(
             json.loads(response.content.decode('utf-8')),
             [
@@ -37,6 +37,58 @@ class GengeAPITest(APITestCase):
                     "location": "8:28452738-28466870",
                     "species": "danio_rerio",
                     "stable_id": "ENSDARG00000042732"
+                }
+            ]
+        )
+
+    def test_get_returns_right_entries2(self):
+        url = reverse('geneautocomplete-list', kwargs={
+            'query': 'mem', 'species': 'danio_rerio', 'limit': 3
+        })
+        response = self.client.get(url)
+
+        self.assertEqual(
+            json.loads(response.content.decode('utf-8')),
+            [
+                {
+                    "db": "core",
+                    "display_label": "tmem267",
+                    "location": "10:13279079-13287169",
+                    "species": "danio_rerio",
+                    "stable_id": "ENSDARG00000000142"
+                },
+                {
+                    "db": "core",
+                    "display_label": "tmem59l",
+                    "location": "22:21128113-21150845",
+                    "species": "danio_rerio",
+                    "stable_id": "ENSDARG00000003655"
+                },
+                {
+                    "db": "core",
+                    "display_label": "tmem63c",
+                    "location": "17:44780166-44923661",
+                    "species": "danio_rerio",
+                    "stable_id": "ENSDARG00000004158"
+                }
+            ]
+        )
+
+    def test_get_returns_right_entries3(self):
+        url = reverse('geneautocomplete-list', kwargs={
+            'query': 'tmem267', 'species': 'danio_rerio', 'limit': 3
+        })
+        response = self.client.get(url)
+
+        self.assertEqual(
+            json.loads(response.content.decode('utf-8')),
+            [
+                {
+                    "db": "core",
+                    "display_label": "tmem267",
+                    "location": "10:13279079-13287169",
+                    "species": "danio_rerio",
+                    "stable_id": "ENSDARG00000000142"
                 }
             ]
         )
